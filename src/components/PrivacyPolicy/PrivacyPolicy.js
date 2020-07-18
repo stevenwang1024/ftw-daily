df925c2ce09bd9c1bdd75fd9a56baca1e003e932
dorazhaojh <dorazhaojh@gmail.com>
2020-07-18 16:35:02 -0400

    LandingPage

--- src/components/PrivacyPolicy/PrivacyPolicy.js
+++ src/components/PrivacyPolicy/PrivacyPolicy.js
@@ -11,51 +11,73 @@ const PrivacyPolicy = props => {
   // prettier-ignore
   return (
     <div className={classes}>
-      <p className={css.lastUpdated}>Last updated: October 30, 2017</p>
+      <p className={css.lastUpdated}>Last updated: Jul 18, 2020</p>
 
       <p>
-        Thank you for using Saunatime! Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Protecting your private information is our priority. This Statement of Privacy applies to dzmarketplace.com and DZ Marketplace LLC and governs data collection and usage. For the purposes of this Privacy Policy, unless otherwise noted, all references to DZ Marketplace LLC include dzmarketplace.com and DZM. By using the DZM website, you consent to the data practices described in this statement. 
       </p>
 
-      <h2>1 Lorem ipsum dolor sit amet</h2>
+      <h2>Collection of your Personal Information</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      In order to better provide you with products and services offered on our Site, DZM may collect personally identifiable information, such as your: 
+      
+      -	First and Last Name 
+      -	E-mail Address 
+      -	Phone Number 
+      -	WeChat 
+      
+      We do not collect any personal information about you unless you voluntarily provide it to us. However, you may be required to provide certain personal information to us when you elect to use certain products or services available on the Site. These may include: (a) registering for an account on our Site; (b) entering a sweepstakes or contest sponsored by us or one of our partners; (c) signing up for special offers from selected third parties; (d) sending us an email message; (e) submitting your credit card or other payment information when ordering and purchasing products and services on our Site. To wit, we will use your information for, but not limited to, communicating with you in relation to services and/or products you have requested from us. We also may gather additional personal or non-personal information in the future. 
+ 
       </p>
 
-      <h2>2 Sed ut perspiciatis unde</h2>
+      <h2>Use of your Personal Information </h2>
       <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
-        dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-        consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
-        molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+      DZM collects and uses your personal information to operate its website(s) and deliver the services you have requested. 
+      DZM may also use your personally identifiable information to inform you of other products or services available from DZM and its affiliates.   
       </p>
 
-      <h2>3 At vero eos et accusamus</h2>
+      <h2>Sharing Information with Third Parties </h2>
       <p>
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-        voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
-        cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id
-        est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam
-        libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod
-        maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
-        Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
-        et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a
-        sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis
-        doloribus asperiores repellat
+      DZM does not sell, rent or lease its customer lists to third parties. 
+      DZM may, from time to time, contact you on behalf of external business partners about a particular offering that may be of interest to you. In those cases, your unique personally identifiable information (e-mail, name, address, telephone number) is transferred to the third party. DZM may share data with trusted partners to help perform statistical analysis, send you email or postal mail, provide customer support, or arrange for deliveries. All such third parties are prohibited from using your personal information except to provide these services to DZM, and they are required to maintain the confidentiality of your information. 
+      DZM may disclose your personal information, without notice, if required to do so by law or in the good faith belief that such action is necessary to: (a) conform to the edicts of the law or comply with legal process served on DZM or the site; (b) protect and defend the rights or property of DZM; and/or (c) act under exigent circumstances to protect the personal safety of users of DZM, or the public. 
       </p>
+      <h2>Automatically Collected Information </h2>
+      <p>
+      Information about your computer hardware and software may be automatically collected by DZM. This information can include: your IP address, browser type, domain names, access times and referring website addresses. This information is used for the operation of the service, to maintain quality of the service, and to provide general statistics regarding use of the DZM website. 
+      </p>
+      <h2>Security of your Personal Information</h2>
+      <p>
+      DZM secures your personal information from unauthorized access, use, or disclosure. DZM uses the following methods for this purpose: 
+  
+      -	SSL Protocol 
+   
+      When personal information (such as a credit card number) is transmitted to other websites, it is protected through the use of encryption, such as the Secure Sockets Layer (SSL) protocol. 
+   
+      We strive to take appropriate security measures to protect against unauthorized access to or alteration of your personal information. Unfortunately, no data transmission over the Internet or any wireless network can be guaranteed to be 100% secure. As a result, while we strive to protect your personal information, you acknowledge that: (a) there are security and privacy limitations inherent to the Internet which are beyond our control; and (b) security, integrity, and privacy of any and all information and data exchanged between you and us through this Site cannot be guaranteed. 
+      </p>
+      <h2>Children Under Thirteen </h2>
+      <p>
+      DZM does not knowingly collect personally identifiable information from children under the age of thirteen. If you are under the age of thirteen, you must ask your parent or guardian for permission to use this website. 
+      </p>
+      <h2>Opt-Out and Unsubscribe from Third Party Communications </h2>
+      <p>
+      We respect your privacy and give you an opportunity to opt-out of receiving announcements of certain information. Users may opt-out of receiving any or all communications from third-party partners of DZM by contacting us here: 
+      - Web page: www.dzmarketplace.com
+      - Email: info@dzmarketplace.com 
+      </p>
+      <h2>E-mail Communications </h2>
+      <p>
+      From time to time, DZM may contact you via email for the purpose of providing announcements, promotional offers, alerts, confirmations, surveys, and/or other general communication. 
+  
+      If you would like to stop receiving marketing or promotional communications via email from DZM, you may opt out of such communications by clicking on the unsubscribe button. 
+      </p>
+      <h2>External Data Storage Sites</h2>
+      <p>We may store your data on servers provided by third party hosting vendors with whom we have contracted. </p>
+      <h2>Changes to this Statement </h2>
+      <p>DZM reserves the right to change this Privacy Policy from time to time. We will notify you about significant changes in the way we treat personal information by sending a notice to the primary email address specified in your account, by placing a prominent notice on our site, and/or by updating any privacy information on this page. Your continued use of the Site and/or Services available through this Site after such modifications will constitute your: (a) acknowledgment of the modified Privacy Policy; and (b) agreement to abide and be bound by that Policy. </p>
+      <h2>Contact Information </h2>
+      <p>DZM welcomes your questions or comments regarding this Statement of Privacy. If you believe that DZM has not adhered to this Statement, please contact DZM at: dora@dzmarketplace.com</p>
     </div>
   );
 };
--- src/components/TermsOfService/TermsOfService.js
+++ src/components/TermsOfService/TermsOfService.js
@@ -11,50 +11,148 @@ const TermsOfService = props => {
   // prettier-ignore
   return (
     <div className={classes}>
-      <p className={css.lastUpdated}>Last updated: October 30, 2017</p>
+      <p className={css.lastUpdated}>Last updated: Jul 30, 2020</p>
 
       <p>
-        Thank you for using Saunatime! Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Welcome to dzmarketplace.com. The dzmarketplace.com website (the "Site") is comprised of 
+      various web pages operated by DZ Marketplace LLC ("DZM"). dzmarketplace.com is offered to you 
+      conditioned on your acceptance without modification of the terms, conditions, and notices 
+      contained herein (the "Terms"). Your use of dzmarketplace.com constitutes your agreement to 
+      all such Terms. Please read these terms carefully, and keep a copy of them for your reference.
+      dzmarketplace.com is a News and Information Site. 
+  
+      The website provide information about services that the company offers. The website also 
+      engages customers, and generates leads and builds brand awareness.
+ 
       </p>
 
-      <h2>1 Lorem ipsum dolor sit amet</h2>
+      <h2>Privacy</h2>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      Your use of dzmarketplace.com is subject to DZM's Privacy Policy. Please review our Privacy 
+      Policy, which also governs the Site and informs users of our data collection practices.
       </p>
 
-      <h2>2 Sed ut perspiciatis unde</h2>
+      <h2>Electronic Communications </h2>
       <p>
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-        laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-        aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
-        dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-        exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi
-        consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil
-        molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
+      Visiting dzmarketplace.com or sending emails to DZM constitutes electronic communications. 
+      You consent to receive electronic communications and you agree that all agreements, notices, 
+      disclosures and other communications that we provide to you electronically, via email and 
+      on the Site, satisfy any legal requirement that such communications be in writing. 
       </p>
 
-      <h2>3 At vero eos et accusamus</h2>
+      <h2>Children Under Thirteen </h2>
       <p>
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-        voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati
-        cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id
-        est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam
-        libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod
-        maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
-        Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut
-        et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a
-        sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis
-        doloribus asperiores repellat
+      DZM does not knowingly collect, either online or offline, personal information from persons 
+      under the age of thirteen. If you are under 18, you may use dzmarketplace.com only with 
+      permission of a parent or guardian. 
+      </p>
+
+      <h2>Links to Third Party Sites/Third Party Services </h2>
+      <p>
+      dzmarketplace.com may contain links to other websites ("Linked Sites"). The Linked Sites are 
+      not under the control of DZM and DZM is not responsible for the contents of any Linked Site, 
+      including without limitation any link contained in a Linked Site, or any changes or updates to 
+      a Linked Site. DZM is providing these links to you only as a convenience, and the inclusion of 
+      any link does not imply endorsement by DZM of the site or any association with its operators. 
+  
+      Certain services made available via dzmarketplace.com are delivered by third party sites and 
+      organizations. By using any product, service or functionality originating from the 
+      dzmarketplace.com domain, you hereby acknowledge and consent that DZM may share such 
+      information and data with any third party with whom DZM has a contractual relationship to 
+      provide the requested product, service or functionality on behalf of dzmarketplace.com users 
+      and customers. 
+      </p>
+
+      <h2>No Unlawful or Prohibited Use/Intellectual Property </h2>
+      <p>
+      You are granted a non-exclusive, non-transferable, revocable license to access and use 
+      dzmarketplace.com strictly in accordance with these terms of use. As a condition of your 
+      use of the Site, you warrant to DZM that you will not use the Site for any purpose that is 
+      unlawful or prohibited by these Terms. You may not use the Site in any manner which could 
+      damage, disable, overburden, or impair the Site or interfere with any other party's use and 
+      enjoyment of the Site. You may not obtain or attempt to obtain any materials or information 
+      through any means not intentionally made available or provided for through the Site. 
+  
+      All content included as part of the Service, such as text, graphics, logos, images, as well as 
+      the compilation thereof, and any software used on the Site, is the property of DZM or its 
+      suppliers and protected by copyright and other laws that protect intellectual property and 
+      proprietary rights. You agree to observe and abide by all copyright and other proprietary 
+      notices, legends or other restrictions contained in any such content and will not make any 
+      changes thereto. 
+  
+      You will not modify, publish, transmit, reverse engineer, participate in the transfer or sale, 
+      create derivative works, or in any way exploit any of the content, in whole or in part, found 
+      on the Site. DZM content is not for resale. Your use of the Site does not entitle you to make 
+      any unauthorized use of any protected content, and in particular you will not delete or alter 
+      any proprietary rights or attribution notices in any content. You will use protected content 
+      solely for your personal use, and will make no other use of the content without the express 
+      written permission of DZM and the copyright owner. You agree that you do not acquire any 
+      ownership rights in any protected content. We do not grant you any licenses, express or implied, 
+      to the intellectual property of DZM or our licensors except as expressly authorized by these terms. 
+      </p>
+      <h2>International Users </h2>
+      <p>
+      The Service is controlled, operated and administered by DZM from our offices within the USA. 
+      If you access the Service from a location outside the USA, you are responsible for compliance 
+      with all local laws. You agree that you will not use the DZM Content accessed through 
+      dzmarketplace.com in any country or in any manner prohibited by any applicable laws, restrictions 
+      or regulations. 
+      </p>
+      <h2>Indemnification </h2>
+      <p>
+      You agree to indemnify, defend and hold harmless DZM, its officers, directors, employees, 
+      agents and third parties, for any losses, costs, liabilities and expenses (including reasonable 
+      attorney's fees) relating to or arising out of your use of or inability to use the Site or 
+      services, any user postings made by you, your violation of any terms of this Agreement or your 
+      violation of any rights of a third party, or your violation of any applicable laws, rules or 
+      regulations. DZM reserves the right, at its own cost, to assume the exclusive defense and 
+      control of any matter otherwise subject to indemnification by you, in which event you will 
+      fully cooperate with DZM in asserting any available defenses. 
+      </p>
+      <h2>Arbitration </h2>
+      <p>
+      In the event the parties are not able to resolve any dispute between them arising out of or 
+      concerning these Terms and Conditions, or any provisions hereof, whether in contract, tort, 
+      or otherwise at law or in equity for damages or any other relief, then such dispute shall be 
+      resolved only by final and binding arbitration pursuant to the Federal Arbitration Act, 
+      conducted by a single neutral arbitrator and administered by the American Arbitration 
+      Association, or a similar arbitration service selected by the parties, in a location mutually 
+      agreed upon by the parties. The arbitrator's award shall be final, and judgment may be entered upon it in any court having jurisdiction. In the event that any legal or equitable action, proceeding or arbitration arises out of or concerns these Terms and Conditions, the prevailing party shall be entitled to recover its costs and reasonable attorney's fees. The parties agree to arbitrate all disputes and claims in regards to these Terms and Conditions or any disputes arising as a result of these Terms and Conditions, whether directly or indirectly, including Tort claims that are a result of these Terms and Conditions. The parties agree that the Federal Arbitration Act governs the interpretation and enforcement of this provision. The entire dispute, including the scope and enforceability of this arbitration provision shall be determined by the Arbitrator. This arbitration provision shall survive the termination of these Terms and Conditions. 
+      </p>
+      <h2>Class Action Waiver </h2>
+      <p>
+      Any arbitration under these Terms and Conditions will take place on an individual basis; class arbitrations and class/representative/collective actions are not permitted. THE PARTIES AGREE THAT A PARTY MAY BRING CLAIMS AGAINST THE OTHER ONLY IN EACH'S INDIVIDUAL CAPACITY, AND NOT AS A PLAINTIFF OR CLASS MEMBER IN ANY PUTATIVE CLASS, COLLECTIVE AND/ OR REPRESENTATIVE PROCEEDING, SUCH AS IN THE FORM OF A PRIVATE ATTORNEY GENERAL ACTION AGAINST THE OTHER. Further, unless both you and DZM agree otherwise, the arbitrator may not consolidate more than one person's claims, and may not otherwise preside over any form of a representative or class proceeding. 
+      </p>
+      <h2>Liability Disclaimer </h2>
+      <p>
+      THE INFORMATION, SOFTWARE, PRODUCTS, AND SERVICES INCLUDED IN OR AVAILABLE THROUGH THE SITE MAY INCLUDE INACCURACIES OR TYPOGRAPHICAL ERRORS. CHANGES ARE PERIODICALLY ADDED TO THE INFORMATION HEREIN. DZ MARKETPLACE LLC AND/OR ITS SUPPLIERS MAY MAKE IMPROVEMENTS AND/OR CHANGES IN THE SITE AT ANY TIME. 
+  
+      DZ MARKETPLACE LLC AND/OR ITS SUPPLIERS MAKE NO REPRESENTATIONS ABOUT THE SUITABILITY, RELIABILITY, AVAILABILITY, TIMELINESS, AND ACCURACY OF THE INFORMATION, SOFTWARE, PRODUCTS, SERVICES AND RELATED GRAPHICS CONTAINED ON THE SITE FOR ANY PURPOSE. TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW, ALL SUCH INFORMATION, SOFTWARE, PRODUCTS, SERVICES AND RELATED GRAPHICS ARE PROVIDED "AS IS" WITHOUT WARRANTY OR CONDITION OF ANY KIND. DZ MARKETPLACE LLC AND/OR ITS SUPPLIERS HEREBY DISCLAIM ALL WARRANTIES AND CONDITIONS WITH REGARD TO THIS INFORMATION, SOFTWARE, PRODUCTS, SERVICES AND RELATED GRAPHICS, INCLUDING ALL IMPLIED WARRANTIES OR CONDITIONS OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. 
+  
+      TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW, IN NO EVENT SHALL DZ MARKETPLACE LLC AND/OR ITS SUPPLIERS BE LIABLE FOR ANY DIRECT, INDIRECT, PUNITIVE, INCIDENTAL, SPECIAL, CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER INCLUDING, WITHOUT LIMITATION, DAMAGES FOR LOSS OF USE, DATA OR PROFITS, ARISING OUT OF OR IN ANY WAY CONNECTED WITH THE USE OR PERFORMANCE OF THE SITE, WITH THE DELAY OR INABILITY TO USE THE SITE OR RELATED SERVICES, THE PROVISION OF OR FAILURE TO PROVIDE SERVICES, OR FOR ANY INFORMATION, SOFTWARE, PRODUCTS, SERVICES AND RELATED GRAPHICS OBTAINED THROUGH THE SITE, OR OTHERWISE ARISING OUT OF THE USE OF THE SITE, WHETHER BASED ON CONTRACT, TORT, NEGLIGENCE, STRICT LIABILITY OR OTHERWISE, EVEN IF DZ MARKETPLACE LLC OR ANY OF ITS SUPPLIERS HAS BEEN ADVISED OF THE POSSIBILITY OF DAMAGES. BECAUSE SOME STATES/JURISDICTIONS DO NOT ALLOW THE EXCLUSION OR LIMITATION OF LIABILITY FOR CONSEQUENTIAL OR INCIDENTAL DAMAGES, THE ABOVE LIMITATION MAY NOT APPLY TO YOU. IF YOU ARE DISSATISFIED WITH ANY PORTION OF THE SITE, OR WITH ANY OF THESE TERMS OF USE, YOUR SOLE AND EXCLUSIVE REMEDY IS TO DISCONTINUE USING THE SITE. 
+      </p>
+      <h2>Termination/Access Restriction </h2>
+      <p>
+      DZM reserves the right, in its sole discretion, to terminate your access to the Site and the related services or any portion thereof at any time, without notice. To the maximum extent permitted by law, this agreement is governed by the laws of the State of New York and you hereby consent to the exclusive jurisdiction and venue of courts in New York in all disputes arising out of or relating to the use of the Site. Use of the Site is unauthorized in any jurisdiction that does not give effect to all provisions of these Terms, including, without limitation, this section. 
+  
+      You agree that no joint venture, partnership, employment, or agency relationship exists between you and DZM as a result of this agreement or use of the Site. DZM's performance of this agreement is subject to existing laws and legal process, and nothing contained in this agreement is in derogation of DZM's right to comply with governmental, court and law enforcement requests or requirements relating to your use of the Site or information provided to or gathered by DZM with respect to such use. If any part of this agreement is determined to be invalid or unenforceable pursuant to applicable law including, but not limited to, the warranty disclaimers and liability limitations set forth above, then the invalid or unenforceable provision will be deemed superseded by a valid, enforceable provision that most closely matches the intent of the original provision and the remainder of the agreement shall continue in effect. 
+  
+      Unless otherwise specified herein, this agreement constitutes the entire agreement between the user and DZM with respect to the Site and it supersedes all prior or contemporaneous communications and proposals, whether electronic, oral or written, between the user and DZM with respect to the Site. A printed version of this agreement and of any notice given in electronic form shall be admissible in judicial or administrative proceedings based upon or relating to this agreement to the same extent and subject to the same conditions as other business documents and records originally generated and maintained in printed form. It is the express wish to the parties that this agreement and all related documents be written in English. 
+      </p>
+      <h2>Changes to Terms </h2>
+      <p>
+      DZM reserves the right, in its sole discretion, to change the Terms under which dzmarketplace.com is offered. The most current version of the Terms will supersede all previous versions. DZM encourages you to periodically review the Terms to stay informed of our updates. 
+      Contact Us
+      </p>
+      <p>
+      DZM welcomes your questions or comments regarding the Terms: 
+        
+      DZ Marketplace LLC 
+      25 Broadway 
+      New York, New York 10004 
+
+      dora@dzmarketplace.com
+
       </p>
     </div>
   );
